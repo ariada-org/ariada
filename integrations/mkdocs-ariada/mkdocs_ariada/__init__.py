486755956600fbe1c0758d64edf4f6dabb666b9d
Alekszandr Bricskin (Agonist Development AB) <git@ariada.org>
2026-06-23 15:40:17 +0200

    feat: add MkDocs Ariada plugin

---
+++ integrations/mkdocs-ariada/mkdocs_ariada/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .plugin import AriadaMkDocsPlugin
+from .scanner import AriadaScanOptions, scan_mkdocs_site
+
+__all__ = ["AriadaMkDocsPlugin", "AriadaScanOptions", "scan_mkdocs_site"]
