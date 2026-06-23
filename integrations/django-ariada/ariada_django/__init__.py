b730f6f280650ed112b0ad1330ece0ea934adb17
Alekszandr Bricskin (Agonist Development AB) <git@ariada.org>
2026-06-23 14:20:39 +0200

    feat: add Django Ariada scanner core
    Signed-off-by: Alexander Brichkin (Agonist Development AB) <git@ariada.org>

---
+++ integrations/django-ariada/ariada_django/__init__.py
@@ -0,0 +1,5 @@
+"""Django adapter for the Ariada accessibility scanner CLI."""
+
+__all__ = ["__version__"]
+
+__version__ = "0.1.0"
---
+++ integrations/django-ariada/ariada_django/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AriadaDjangoConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ariada_django"
+    verbose_name = "Ariada Django"
